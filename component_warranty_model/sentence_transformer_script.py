fe892f7196d3e903ceaded281e7da62790727780
abbasalit987 <abbasalit987@gmail.com>
2025-01-07 18:00:45 +0530

    NER Model for Haier Brands TV Model extraction

--- component_warranty_model/sentence_transformer_script.py
+++
@@ -1,8 +0,0 @@
-from sentence_transformers import SentenceTransformer, util
-
-model = SentenceTransformer('all-MiniLM-L6-v2')
-embeddings1 = model.encode("UA43AU7700 samsung")
-# embeddings2 = model.encode("SAMSUNG UA43AU7700 - 43\" LED")
-embeddings2 = model.encode("SAMSUNG UA43AU7700 - LED")
-similarity = util.cos_sim(embeddings1, embeddings2)
-print(similarity)
